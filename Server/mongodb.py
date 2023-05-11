0c6e29089a4a442e51940563c696661d1d355a29
Aminafarooq21 <124820331+Aminafarooq21@users.noreply.github.com>
2023-05-11 15:21:05 +0500

    Update mongodb.py

--- Server/mongodb.py
+++ Server/mongodb.py
@@ -21,3 +21,104 @@ def store_preferences(processed_data, collection_name):
     result = collection.insert_one(processed_data)
     
     print(f"Inserted document with ID: {result.inserted_id}")
+    
+    
+  #  CLAM
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+
+# connect to MongoDB database
+client = MongoClient('mongodb://localhost:27017/')
+db = client['clam_db']
+
+# define schema for course and lab data
+course_schema = {
+    'code': str,
+    'title': str,
+    'instructor': str,
+    'day': str,
+    'time': str,
+    'venue': str,
+    'labs': [str],
+    'students': int
+}
+
+lab_schema = {
+    'code': str,
+    'title': str,
+    'instructor': str,
+    'day': str,
+    'time': str,
+    'venue': str,
+    'students': int
+}
+
+# create Flask application
+app = Flask(_name_)
+
+# route for receiving course and lab data
+@app.route('/clam/input', methods=['POST'])
+def receive_input():
+    course_data = request.json['courses']
+    lab_data = request.json['labs']
+    
+    course_collection = db['courses']
+    lab_collection = db['labs']
+    
+    for course in course_data:
+        course_collection.insert_one(course)
+        
+    for lab in lab_data:
+        lab_collection.insert_one(lab)
+    
+    return jsonify({'message': 'Data received.'})
+
+# route for assigning courses to instructors
+@app.route('/clam/assign_courses', methods=['POST'])
+def assign_courses():
+    instructor_name = request.json['instructor_name']
+    course_collection = db['courses']
+    
+    courses = course_collection.find({'instructor': instructor_name})
+    
+    for course in courses:
+        # assign the course to the instructor based on their preferences and availability
+        pass
+    
+    return jsonify({'message': 'Courses assigned.'})
+
+# route for allocating labs to instructors
+@app.route('/clam/allocate_labs', methods=['POST'])
+def allocate_labs():
+    instructor_name = request.json['instructor_name']
+    lab_collection = db['labs']
+    
+    labs = lab_collection.find({'instructor': instructor_name})
+    
+    for lab in labs:
+        # allocate the lab to the instructor based on their preferences and availability
+        pass
+    
+    return jsonify({'message': 'Labs allocated.'})
+
+# route for allocating venues for courses and labs
+@app.route('/clam/allocate_venues', methods=['POST'])
+def allocate_venues():
+    course_collection = db['courses']
+    lab_collection = db['labs']
+    
+    courses = course_collection.find()
+    labs = lab_collection.find()
+    
+    for course in courses:
+        # allocate a suitable venue for the course based on its availability
+        pass
+    
+    for lab in labs:
+        # allocate a suitable venue for the lab based on its availability
+        pass
+    
+    return jsonify({'message': 'Venues allocated.'})
+
+if _name_ == '_main_':
+    app.run(debug=True)
